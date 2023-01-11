9134987bdeb66f1ef4341955816edb000383cb9c
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-01-10 23:27:30 -0600

    Added DockerComposer

--- Projects/Voice Assistant/VoiceAssitant.py
+++ Projects/Voice Assistant/VoiceAssitant.py
@@ -7,15 +7,17 @@ import pyaudio
 
 def SpeechTextRecognizer():
     Recognizer = speech_recognition.Recognizer()
+    audio = ''
     with speech_recognition.Microphone() as Source:
         print("Listening...")
-        Recognizer.adjust_for_ambient_noise(Source)
+        Recognizer.adjust_for_ambient_noise(Source,1)
         Audio = Recognizer.listen(Source)
         try:
             print("Recognizing...")
             Data = Recognizer.recognize_google(Audio)
             print(Data)
         except speech_recognition.UnknownValueError:
-            print(" I Don't Understand")
+            print(" Could not head you, try saying again ")
+    
 
 SpeechTextRecognizer()
