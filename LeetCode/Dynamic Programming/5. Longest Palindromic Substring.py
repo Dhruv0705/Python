d9ac6305428f3654fd1c355431d8980777ec8ae1
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-03-22 00:23:26 -0500

    Update Longest Palindromic Substring

--- LeetCode/Dynamic Programming/5. Longest Palindromic Substring.py
+++ LeetCode/Dynamic Programming/5. Longest Palindromic Substring.py
@@ -64,3 +64,67 @@ s = "babad"
 print(Solution().longestPalindrome(s))
 
 
+class Solution1:
+    def longestPalindrome(self, s: str) -> str:
+
+        # set the result to an empty string
+        result = ""
+
+        # Set the result length to 0
+        resultLength = 0
+
+        # for each character in the string
+        for i in range(len(s)):
+            
+            # set the left and right pointers to the current character
+            left, right = i, i
+
+            # while the left and right pointers are within the bounds of the string
+            # and the characters at the left and right pointers are equal
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                    
+                # if the length of the current substring is greater than the result length
+                if right - left + 1 > resultLength:
+                        
+                    # update the result length to the current substring length
+                    resultLength = right - left + 1
+                        
+                    # update the result to the current substring
+                    result = s[left:right + 1]
+                    
+                # move the left pointer to the left
+                left -= 1
+                    
+                # move the right pointer to the right
+                right += 1
+            
+            # set the left and right pointers to the current character and the next character
+            left, right = i, i + 1
+
+            # while the left and right pointers are within the bounds of the string
+            # and the characters at the left and right pointers are equal
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                    
+                # if the length of the current substring is greater than the result length
+                if right - left + 1 > resultLength:
+                        
+                    # update the result length to the current substring length
+                    resultLength = right - left + 1
+                        
+                    # update the result to the current substring
+                    result = s[left:right + 1]
+                    
+                # move the left pointer to the left
+                left -= 1
+                    
+                # move the right pointer to the right
+                right += 1
+            
+        # return the result
+        return result
+
+# Test the solution
+s = "babad"
+print(Solution1().longestPalindrome(s))
+
+
